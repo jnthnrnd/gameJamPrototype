6dc8489fd7a0d16f748ec0048e4d9b829f567fe3
Nathan <ornido.jonathan@gmail.com>
2020-10-24 22:19:03 +0800

    UI Scripts and Camera Settings
    Added Cinemachine for Camera View of Player
    Simple UI for the game

--- PrototypeGJ/Assets/Script/PlayerControl.cs
+++ PrototypeGJ/Assets/Script/PlayerControl.cs
@@ -5,58 +5,74 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed = 10f, jumpForce = 10f;
+    private float moveSpeed = 6f, jumpHeight = 3f, groundDistance = 0.4f;
 
     [SerializeField]
-    private float horizontalInput, verticalInput, gravityModifier;
+    private float horizontalInput, verticalInput, gravity = -9.81f;
+    
+    [SerializeField]
+    private Transform groundCheck;
 
-    public Rigidbody playerRb;
-    private BoxCollider playerBoxCollider;
-    private bool isOnGround = true;
+    [SerializeField]
+    private LayerMask groundMask;
 
+    public CharacterController controller;
+    public Transform cam;
+
+    private float turnSmoothTime = 0.1f;
+    private Vector3 direction = Vector3.zero;
+
+    float turnSmoothVelocity;
+    Vector3 velocity;
+    bool isOnGround;
 
     // Start is called before the first frame update
     void Start()
     {
-        //get player's rigidbody
-        if(playerRb == null)
-        {
-            playerRb = GetComponent<Rigidbody>();
-        }
 
-        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        isOnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isOnGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         MovePlayer();
         JumpPlayer();
+      
     }
 
     void MovePlayer()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(Vector3.forward * verticalInput * moveSpeed * Time.deltaTime);
-        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+        direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+        }
     }
 
     void JumpPlayer()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            isOnGround = false;
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+          velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
